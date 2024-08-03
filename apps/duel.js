24958451ff13715db83108aa7a385a8494817b1b
CuteMiao Xuwen <3522317086@qq.com>
2024-08-03 09:35:30 +0800

    modified:   apps/duel.js

--- apps/duel.js
+++ apps/duel.js
@@ -5,6 +5,8 @@ import yaml from 'yaml';
 const _path = process.cwd().replace(/\\/g, "/");
 const dataPath = `${_path}/plugins/xunmiao-plugin/data/user_data.yaml`;
 
+const cooldowns = {}; // 存储每个用户的冷却时间
+
 export class duel extends plugin {
   constructor() {
     super({
@@ -14,8 +16,8 @@ export class duel extends plugin {
       priority: 5000,
       rule: [
         {
-            reg: '^#决斗(.*)$',
-            fnc: 'duel'
+          reg: '^#决斗(.*)$',
+          fnc: 'duel'
         }
       ]
     })
@@ -23,6 +25,19 @@ export class duel extends plugin {
 
   async duel(e) {
     if (e.isGroup) {
+        const now = Date.now();
+        const cooldownTime = 60 * 1000; // 1分钟的冷却时间
+
+        // 检查发起者的冷却时间
+        if (cooldowns[e.user_id]) {
+            const timePassed = now - cooldowns[e.user_id];
+            const timeLeft = cooldownTime - timePassed;
+            if (timeLeft > 0) {
+                const secondsLeft = Math.ceil(timeLeft / 1000);
+                return e.reply(`你需要等待${secondsLeft}秒后才能再次发起决斗哦~`, false, { at: true });
+            }
+        }
+
         const fileContent = fs.readFileSync(dataPath, 'utf8');
         let userData = yaml.parse(fileContent) || {};
 
@@ -39,8 +54,8 @@ export class duel extends plugin {
             };
         }
 
-        const {coins: coins_id1} = userData[e.user_id];
-        const {coins: coins_id2} = userData[user_id2];
+        const { coins: coins_id1 } = userData[e.user_id];
+        const { coins: coins_id2 } = userData[user_id2];
 
         let message = e.message;
 
@@ -49,18 +64,18 @@ export class duel extends plugin {
         console.log(user_id2);
 
         if (message.some(item => item.qq === '2582312528')) {
-            return e.reply('你不能与我进行决斗哦~', false, { at:true });
+            return e.reply('你不能与我进行决斗哦~', false, { at: true });
         }
 
         if (e.user_id == user_id2) {
-            return e.reply('你不能与自己进行决斗哦~', false, { at:true });
+            return e.reply('你不能与自己进行决斗哦~', false, { at: true });
         }
 
         if (coins_id1 <= 0) {
-            return e.reply('你的喵喵币不够哦，无法与他人决斗~', false, { at:true });
+            return e.reply('你的喵喵币不够哦，无法与他人决斗~', false, { at: true });
         }
         if (coins_id2 <= 0) {
-            return e.reply('他的喵喵币不够哦，无法与他决斗~', false, { at:true });
+            return e.reply('他的喵喵币不够哦，无法与他决斗~', false, { at: true });
         }
 
         let i = Math.floor(Math.random() * 100);
@@ -68,27 +83,30 @@ export class duel extends plugin {
 
         if (i <= 50) {
             e.reply(`你输了! 你损失了${Duelcoins}个喵喵币
-他获得了${Duelcoins}个喵喵币`, false, { at:true });
+他获得了${Duelcoins}个喵喵币`, false, { at: true });
 
             userData[user_id2].coins += Duelcoins;
             userData[e.user_id].coins -= Duelcoins;
         } else {
             e.reply(`你赢了! 你获得了${Duelcoins}个喵喵币
-他损失了${Duelcoins}个喵喵币`, false, { at:true });
+他损失了${Duelcoins}个喵喵币`, false, { at: true });
 
             userData[user_id2].coins -= Duelcoins;
             userData[e.user_id].coins += Duelcoins;
         }
 
-        if (userData[user_id2].coins <=0 ) {
-          userData[user_id2].coins = 0
+        if (userData[user_id2].coins <= 0) {
+            userData[user_id2].coins = 0;
         }
 
-        if (userData[e.user_id].coins <=0 ) {
-          userData[e.user_id].coins = 0
+        if (userData[e.user_id].coins <= 0) {
+            userData[e.user_id].coins = 0;
         }
 
         fs.writeFileSync(dataPath, yaml.stringify(userData));
+
+        // 更新发起者的冷却时间
+        cooldowns[e.user_id] = now;
     }
   }
 }
