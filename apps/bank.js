2e7e92d8a17919a4c2a06de3abf4928690594d6b
XuwenMeimei <3522317086@qq.com>
2025-06-29 19:21:44 +0800

    正则多样性

--- apps/bank.js
+++ apps/bank.js
@@ -20,11 +20,11 @@ export class duel extends plugin {
       priority: 5000,
       rule: [
         {
-          reg: '^#存钱(.*)$',
+          reg: '^#存钱|存入(.*)$',
           fnc: 'save'
         },
         {
-        reg: '^#取钱(.*)$',
+        reg: '^#取钱|取出(.*)$',
         fnc: 'take'
         }
       ]
