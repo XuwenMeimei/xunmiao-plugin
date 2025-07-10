8692c2762122d0958d89cb6a3db72e9aa8cf3a4b
XuwenMeimei <3522317086@qq.com>
2025-07-10 22:12:37 +0800

    123

--- apps/marry.js
+++ apps/marry.js
@@ -100,10 +100,9 @@ export class marry extends plugin {
             `向你求婚：‘亲爱的${sex}您好！`, "\n",
             `在茫茫人海中，能够与${sex}相遇相知相恋，我深感幸福，守护你是我今生的选择，我想有个自己的家，一个有你的家,嫁给我好吗？’`, "\n",
             segment.at(atUserId), "\n",
-            `那么这位${sex}，你愿意嫁给ta吗？at并发送【我愿意】或者【我拒绝】，回应${she_he}哦！`
-        ])
-        return;
+            `那么这位${sex}，你愿意嫁给ta吗？at并发送【我愿意】或者【我拒绝】，回应${she_he}哦！`]);
         }
+        return;
     }
 
     async accept(e) {
