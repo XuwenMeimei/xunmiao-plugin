a4ab1ec38ae8b539327a534f40dc5767ed8330b4
XuwenMeimei <3522317086@qq.com>
2025-07-10 19:48:11 +0800

    结婚功能

---
+++ apps/marry.js
@@ -0,0 +1,56 @@
+import plugin from '../../../lib/plugins/plugin.js';
+
+const _path = process.cwd().replace(/\\/g, '/');
+const marryDataPath = `${_path}/plugins/xunmiao-plugin/data/marry_data.yaml`;
+
+function getMarryData() {
+    if (!fs.existsSync(marryDataPath)) fs.writeFileSync(marryDataPath, yaml.stringify({}));
+    return yaml.parse(fs.readFileSync(marryDataPath, 'utf8')) || {};
+}
+
+
+export class marry extends plugin {
+  constructor() {
+    super({
+      name: '结婚',
+      dsc: '结婚娱乐模块',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^#结婚$',
+          fnc: 'marry'
+        }
+      ]
+    });
+  }
+
+  async marry(e) {
+    if (e.isGroup) {
+        const marryData = getMarryData();
+        const userId = e.user_id;
+        const atUserId = e.at;
+
+        if (!atUserId) {
+            return e.reply('请@你想要结婚的人哦~', false, { at: true });
+        }
+
+        if (userId === atUserId) {
+            return e.reply('你不能和自己结婚哦~', false, { at: true });
+        }
+
+        if (marryData[userId] && marryData[userId].married) {
+            return e.reply('你已经结婚了哦~', false, { at: true });
+        }
+
+        if (marryData[atUserId] && marryData[atUserId].married) {
+            return e.reply('对方已经结婚了哦~', false, { at: true });
+        }
+
+        return e.reply('咕咕咕', false, { at: true })
+    }
+    else {
+        return e.reply('这个功能只能在群聊中使用哦~', false, { at: false });
+    }
+  }
+}
--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const userDailyBuyPath = path.join(dataDir, 'user_daily_buy.yaml')
 const deepseek_config = path.join(__dirname, 'config/deepseek/config.template.yaml')
 const deepseek_config_path = path.join(__dirname, 'config/deepseek/config.yaml')
 
+const marry_data = path.join(dataDir, 'marry_data.yaml')
+const marry_data_path = path.join(dataDir, 'marry_data.template.yaml')
+
+
+//初始化 marry_data 配置文件
+if (!fs.existsSync(marry_data)) {
+  if (fs.existsSync(marry_data_path)) {
+    fs.copyFileSync(marry_data_path, marry_data)
+    console.log('从模板复制了 marry_data.yaml')
+  } else {
+    console.warn('未找到模板文件：', marry_data_path)
+  }
+}
+
 // 初始化 DeepSeek 配置文件
 if (!fs.existsSync(deepseek_config_path)) {
   if (fs.existsSync(deepseek_config)) {
