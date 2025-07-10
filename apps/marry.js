1718db45f52e5db93152764912e12eab164ad8af
XuwenMeimei <3522317086@qq.com>
2025-07-10 21:27:09 +0800

    123

--- apps/marry.js
+++ apps/marry.js
@@ -68,8 +68,25 @@ export class marry extends plugin {
             return e.reply([segment.at(userId), ' 对方已经结婚了哦~ ']);
         }
 
+        let she_he = await this.people(e, 'sex', userId);
+
         if (!marryData[userId].married) {
-            
+            let sex = '';
+            if (sex == 'male') {
+                sex = '小姐';
+            } else if (sex == 'female') {
+                sex = '先生';
+            }
+            e.reply([
+            segment.at(e.at), "\n",
+            segment.image(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${atUserId}`), "\n",
+            segment.at(id), "\n",
+            segment.image(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${userId}`), "\n",
+            `向你求婚：‘亲爱的${sex}您好！`, "\n",
+            `在茫茫人海中，能够与${sex}相遇相知相恋，我深感幸福，守护你是我今生的选择，我想有个自己的家，一个有你的家,嫁给我好吗？’`, "\n",
+            segment.at(atUserId), "\n",
+            `那么这位${sex}，你愿意嫁给ta吗？at并发送【我愿意】或者【我拒绝】，回应${she_he}哦！`,
+        ])
         }
     }
   }
