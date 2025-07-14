82ddd91b3f007fe71807fde13f9569d24c02852b
XuwenMeimei <3522317086@qq.com>
2025-07-14 23:25:07 +0800

    123

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import plugin from '../../lib/plugins/plugin.js'
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
